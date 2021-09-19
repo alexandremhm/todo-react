fefcb36a935f12a85a2e8aef788fbc18cc127db3
Matheus Alexandre <alexandremhm@gmail.com>
2021-09-19 16:42:09 -0300

    Add remove task

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,15 +2,20 @@ import React, { useContext } from 'react';
 import notesContext from '../context/notesContext';
 
 function TaskList () {
-  const { notes } = useContext(notesContext);
+  const { notes, removeNote } = useContext(notesContext);
   return (
     <div>
       <ul>
         {
           notes.map((note, index) => (
-            <li key={index}>
-              {note.task}
-            </li>
+            <div key={index}>              
+              <li>
+                {note.task}
+              </li>
+              <button onClick={() => removeNote(note.id)}>
+                Delete
+              </button>
+            </div>
           ))
         }
       </ul>
